983b9f24da3dab257af9c5615fc68cdef58d5d85
TOBIAS ZASELSKY <47435897@ort>
2023-06-28 14:30:27 -0300

    inicialice el proyecto

---
+++ src/Components/Titulo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Titulo = () => {
+  return (
+    <header className='header'>
+        <h1 className='header__titulo'>Trabajos de Arte</h1>
+        <h3 className='header__subtitulo'>Informatica D</h3>
+    </header>
+  )
+}
+
+export default Titulo
---
+++ src/Components/Trabajos.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import TrabajosL from "../Trabajos.json" 
+
+const Trabajos = () => {
+
+
+
+  return (
+
+    {
+        TrabajosL.forEach(element => {
+            
+        });
+    }
+  )
+}
+
+export default Trabajos
